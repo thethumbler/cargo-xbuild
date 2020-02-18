1663a254cf1d7aece4d72ae45b0b99a97b5c9407
Andrew Jones <ascjones@gmail.com>
2020-02-18 13:54:44 +0000

    Improve passing explicit args for lib usage

--- src/cli.rs
+++ src/cli.rs
@@ -5,21 +5,114 @@ pub struct Args {
     all: Vec<String>,
     target: Option<String>,
     manifest_path: Option<PathBuf>,
+    verbosity: Option<Verbosity>,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Verbosity {
+    Quiet,
+    Verbose,
 }
 
 impl Args {
-    pub fn new<A, S, T, P>(all: A, target: Option<T>, manifest_path: Option<P>) -> Self
+    /// Create args explicitly, with other args passed unchanged to cargo invocation
+    pub fn new<T, P, A, S>(
+        target: Option<T>,
+        manifest_path: Option<P>,
+        verbosity: Option<Verbosity>,
+        other_args: A
+    ) -> Result<Self, String>
+    where
+        T: Into<String> + Clone,
+        P: AsRef<Path>,
+        A: IntoIterator<Item = S>,
+        S: AsRef<str>,
+    {
+        // check for duplicates of the explicit args
+        let explicit_args = ["--target", "--manifest-path", "--verbose", "--quiet"];
+        let other_args = other_args
+            .into_iter()
+            .map(|a| a.as_ref().to_string())
+            .collect::<Vec<_>>();
+
+        let duplicates = other_args
+            .iter()
+            .filter(|a| explicit_args.iter().any(|ea| a.starts_with(ea)))
+            .collect::<Vec<_>>();
+        if !duplicates.is_empty() {
+            return Err(
+                format!("The following args should be passed explicitly: '{:?}'", duplicates)
+            )
+        }
+
+        // add the explicitly specified args to `all` which will be passed on to `cargo`
+        let mut all = other_args;
+        if let Some(target) = target.clone() {
+            all.push(format!("--target={}", target.into()))
+        }
+        if let Some(ref manifest_path) = manifest_path {
+            all.push(format!("--manifest-path={}", manifest_path.as_ref().to_string_lossy()))
+        }
+        if let Some(ref verbosity) = verbosity {
+            match verbosity {
+                Verbosity::Verbose => all.push("--verbose".into()),
+                Verbosity::Quiet => all.push("--quiet".into()),
+            }
+        }
+
+        Ok(Args {
+            all,
+            target: target.map(Into::into),
+            manifest_path: manifest_path.map(|p| p.as_ref().into()),
+            verbosity,
+        })
+    }
+
+    /// Parse raw args from command line
+    pub fn from_raw<A, S>(all: A) -> Result<Self, String>
     where
         A: IntoIterator<Item = S>,
         S: AsRef<str>,
-        T: Into<String>,
-        P: Into<PathBuf>,
     {
-        Args {
-            all: all.into_iter().map(|s| s.as_ref().to_string()).collect(),
-            target: target.map(Into::into),
-            manifest_path: manifest_path.map(Into::into)
+        let all = all.into_iter().map(|a| a.as_ref().to_string()).collect::<Vec<_>>();
+
+        let mut target: Option<String> = None;
+        let mut manifest_path = None;
+        let mut verbosity = None;
+        {
+            let mut args = all.iter();
+            while let Some(arg) = args.next() {
+                if arg == "--target" {
+                    target = args.next().map(|s| s.to_owned());
+                } else if arg.starts_with("--target=") {
+                    target = arg.splitn(2, '=').nth(1).map(|s| s.to_owned());
+                }
+                if arg == "--manifest-path" {
+                    manifest_path = args.next().map(|s| s.to_owned());
+                } else if arg.starts_with("--manifest-path=") {
+                    manifest_path = arg.splitn(2, '=').nth(1).map(|s| s.to_owned());
+                }
+                if arg == "--verbose" || arg == "-v" || arg == "-vv" {
+                    if let Some(Verbosity::Quiet) = verbosity {
+                        return Err("cannot set both --verbose and --quiet".into())
+                    }
+                    verbosity = Some(Verbosity::Verbose)
+                }
+                if arg == "--quiet" || arg == "-q" {
+                    if let Some(Verbosity::Verbose) = verbosity {
+                        return Err("cannot set both --verbose and --quiet".into())
+                    }
+                    verbosity = Some(Verbosity::Quiet)
+                }
+            }
         }
+
+        Ok(Args {
+            all,
+            target: target.map(Into::into),
+            manifest_path: manifest_path.map(Into::into),
+            verbosity,
+        })
     }
 
     pub fn all(&self) -> &[String] {
@@ -35,13 +128,11 @@ impl Args {
     }
 
     pub fn quiet(&self) -> bool {
-        self.all.iter().any(|a| a == "--quiet" || a == "-q")
+        self.verbosity == Some(Verbosity::Quiet)
     }
 
     pub fn verbose(&self) -> bool {
-        self.all
-            .iter()
-            .any(|a| a == "--verbose" || a == "-v" || a == "-vv")
+        self.verbosity == Some(Verbosity::Verbose)
     }
 }
 
@@ -60,25 +151,7 @@ pub fn args(command_name: &str) -> Result<(Command, Args), String> {
         _ => Command::Build,
     };
 
-    let mut target: Option<String> = None;
-    let mut manifest_path = None;
-    {
-        let mut args = all.iter();
-        while let Some(arg) = args.next() {
-            if arg == "--target" {
-                target = args.next().map(|s| s.to_owned());
-            } else if arg.starts_with("--target=") {
-                target = arg.splitn(2, '=').nth(1).map(|s| s.to_owned());
-            }
-            if arg == "--manifest-path" {
-                manifest_path = args.next().map(|s| s.to_owned());
-            } else if arg.starts_with("--manifest-path=") {
-                manifest_path = arg.splitn(2, '=').nth(1).map(|s| s.to_owned());
-            }
-        }
-    }
-
-    let args = Args::new(all, target, manifest_path);
+    let args = Args::from_raw(all)?;
     Ok((command, args))
 }
 
